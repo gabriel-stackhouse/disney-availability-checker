cef9c2896bedabb4976ed0eb1f89ec89bec3526a
Gabriel Stackhouse <gstacks1322@gmail.com>
2021-06-23 01:57:11 -0500

    send alert with availability, run headless, and polish

--- constant.py
+++ constant.py
@@ -1,2 +1,4 @@
 DISNEY_AVAILABILITY_WEBSITE = "https://disneyworld.disney.go.com/availability-calendar/?segments=tickets,resort," \
                               "passholder&defaultSegment=tickets "
+CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                    "Chrome/91.0.4472.106 Safari/537.36"
--- disney-availability-checker.py
+++ disney-availability-checker.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
+from pushover import Client
 from parks import Park
 import constant
 import time
@@ -9,18 +10,24 @@ import argparse
 
 
 def main():
-    args = parse_and_validate_arguments()
-    driver = build_chrome_driver()
-    driver.get(constant.DISNEY_AVAILABILITY_WEBSITE)  # todo - possibly change this to a property
-    time.sleep(3)  # todo - change to something better
-    navigate_to_date(driver, args.date)
-    date_string = args.date.strftime("%b %#d")
-    if is_park_available(driver, args.park):
-        message = args.park.value + " is available on " + date_string + "! Hurry up and reserve it!!!"
-        send_pushbullet_alert(message)
-        print(message)
-    else:
-        print(args.park.value + " is not available on " + date_string + ".")
+    try:
+        args = parse_and_validate_arguments()
+        driver = build_chrome_driver()
+        driver.get(constant.DISNEY_AVAILABILITY_WEBSITE)
+        time.sleep(3)  # todo - change to something better
+        navigate_to_date(driver, args.date)
+        date_string = args.date.strftime("%B %#d")
+        if is_park_available(driver, args.park):
+            title = args.park.value + " is available!"
+            message = args.park.value + " is available on " + date_string + "! Hurry up and reserve it!!!"
+            send_pushbullet_alert(title, message)
+            print(message)
+        else:
+            print(args.park.value + " is not available on " + date_string + ".")
+    except Exception as e:
+        send_pushbullet_alert("Error in Disney Availability Script",
+                              "Something's going wrong with disney-availability-checker. Better go check it out.")
+        raise e
 
 
 def parse_and_validate_arguments():
@@ -34,23 +41,46 @@ def parse_and_validate_arguments():
                         help="date to check for availability, in format MM-dd-yyyy")
     args = parser.parse_args()
     if args.date < date.today():
-        print("Date is in the past, dummy!")
-        exit(1)
+        raise ValueError("Date is in the past, dummy!")
     elif args.date > date.today() + relativedelta(years=1):
-        print("Script only supports checking availability for a year and under")
-        exit(1)
+        raise ValueError("Script only supports checking availability for a year and under")
     return args
 
 
 def build_chrome_driver():
-    options = Options()
-    # options.headless = True   # todo - uncomment
-    options.add_argument("--log-level=3")
-    return webdriver.Chrome(options=options)
+    chrome_options = Options()
+    chrome_options.headless = True
+    chrome_options.add_argument("--user-agent=" + constant.CHROME_USER_AGENT)
+    chrome_options.add_argument("--log-level=3")
+    return webdriver.Chrome(options=chrome_options)
 
 
-def expand_shadow_element(driver, element):
-    return driver.execute_script("return arguments[0].shadowRoot", element)
+def navigate_to_date(driver, date_to_navigate):
+    calendar = find_calendar(driver)
+    calendar_nav = expand_shadow_element(driver, calendar)
+    month_header = calendar_nav.find_element_by_id("monthHeader")
+    while month_header.text != date_to_navigate.strftime("%B"):
+        calendar_nav.find_element_by_id("nextArrow").click()
+        time.sleep(2)  # todo - change to something better
+    date_buttons = calendar.find_elements_by_tag_name("wdat-date")
+    for date_button in date_buttons:
+        if date_button.get_attribute("slot") == date_to_navigate.isoformat():
+            date_button.click()
+            time.sleep(1.5)  # todo - change to something better
+            return
+
+
+def is_park_available(driver, park_to_check):
+    park_availabilities = find_park_availabilities(driver)
+    for park_availability in park_availabilities:
+        park_name = park_availability.find_elements_by_tag_name("span")[0].text
+        if park_to_check.value in park_name:
+            return park_availability.get_attribute("class") == "available"
+    return False
+
+
+def send_pushbullet_alert(title, message):
+    Client().send_message(message, title=title)
 
 
 def find_calendar(driver):
@@ -66,37 +96,9 @@ def find_park_availabilities(driver):
     return park_availability_container.find_elements_by_tag_name("div")
 
 
-# todo - make this actually navigate to the correct date
-def navigate_to_date(driver, date_to_check):
-    calendar = find_calendar(driver)
-    calendar_nav = expand_shadow_element(driver, calendar)
-    month_header = calendar_nav.find_element_by_id("monthHeader")
-    while month_header.text != date_to_check.strftime("%B"):
-        calendar_nav.find_element_by_id("nextArrow").click()
-        time.sleep(2)  # todo - change to something better
-    date_buttons = calendar.find_elements_by_tag_name("wdat-date")
-    for date_button in date_buttons:
-        if date_button.get_attribute("slot") == date_to_check.isoformat():
-            date_button.click()  # todo - what happens when button isn't clickable?
-            time.sleep(1)  # todo - change to something better
-            return
+def expand_shadow_element(driver, element):
+    return driver.execute_script("return arguments[0].shadowRoot", element)
 
 
-def is_park_available(driver, park_to_check):
-    park_availabilities = find_park_availabilities(driver)
-    for park_availability in park_availabilities:
-        park_name = park_availability.find_elements_by_tag_name("span")[0].text
-        if park_to_check.value in park_name:
-            return park_availability.get_attribute("class") == "available"
-    return False
-
-
-def send_pushbullet_alert(message):
-    # todo - do something
-    return
-
-
-# Start script
 if __name__ == "__main__":
     main()
-
