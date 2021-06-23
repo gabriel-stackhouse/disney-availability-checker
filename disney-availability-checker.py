d2d9f0ca6bfbb1558dcd7c8caa48d2c6c9bc2eae
Gabriel Stackhouse <gstacks1322@gmail.com>
2021-06-23 00:00:09 -0500

    take in arguments to specify the park and date to check

--- disney-availability-checker.py
+++ disney-availability-checker.py
@@ -1,28 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from parks import Parks
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from parks import Park
 import constant
 import time
-import datetime
+import argparse
 
 
 def main():
-    park_to_check = Parks.HOLLYWOOD_STUDIOS  # todo - use passed-in argument
+    args = parse_and_validate_arguments()
     driver = build_chrome_driver()
     driver.get(constant.DISNEY_AVAILABILITY_WEBSITE)  # todo - possibly change this to a property
     time.sleep(3)  # todo - change to something better
-    navigate_to_date(driver)
-    if is_park_available(driver, park_to_check):
-        message = park_to_check.pretty_name + " is available on July 4th! Hurry up and reserve it!!!"
+    navigate_to_date(driver, args.date)
+    date_string = args.date.strftime("%b %#d")
+    if is_park_available(driver, args.park):
+        message = args.park.value + " is available on " + date_string + "! Hurry up and reserve it!!!"
         send_pushbullet_alert(message)
         print(message)
     else:
-        print(park_to_check.pretty_name + " is not available on July 4th.")
+        print(args.park.value + " is not available on " + date_string + ".")
+
+
+def parse_and_validate_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("park",
+                        type=Park,
+                        choices=list(Park),
+                        help="the Disney park to check for availability")
+    parser.add_argument("date",
+                        type=lambda s: datetime.strptime(s, '%m-%d-%Y').date(),
+                        help="date to check for availability, in format MM-dd-yyyy")
+    args = parser.parse_args()
+    if args.date < date.today():
+        print("Date is in the past, dummy!")
+        exit(1)
+    elif args.date > date.today() + relativedelta(years=1):
+        print("Script only supports checking availability for a year and under")
+        exit(1)
+    return args
 
 
 def build_chrome_driver():
     options = Options()
     # options.headless = True   # todo - uncomment
+    options.add_argument("--log-level=3")
     return webdriver.Chrome(options=options)
 
 
@@ -44,15 +67,16 @@ def find_park_availabilities(driver):
 
 
 # todo - make this actually navigate to the correct date
-def navigate_to_date(driver):
+def navigate_to_date(driver, date_to_check):
     calendar = find_calendar(driver)
     calendar_nav = expand_shadow_element(driver, calendar)
-    if calendar_nav.find_element_by_id("monthHeader").text == "June":
+    month_header = calendar_nav.find_element_by_id("monthHeader")
+    while month_header.text != date_to_check.strftime("%B"):
         calendar_nav.find_element_by_id("nextArrow").click()
-        time.sleep(1.5)  # todo - change to something better
+        time.sleep(2)  # todo - change to something better
     date_buttons = calendar.find_elements_by_tag_name("wdat-date")
     for date_button in date_buttons:
-        if date_button.get_attribute("slot") == "2021-07-04":
+        if date_button.get_attribute("slot") == date_to_check.isoformat():
             date_button.click()  # todo - what happens when button isn't clickable?
             time.sleep(1)  # todo - change to something better
             return
@@ -62,7 +86,7 @@ def is_park_available(driver, park_to_check):
     park_availabilities = find_park_availabilities(driver)
     for park_availability in park_availabilities:
         park_name = park_availability.find_elements_by_tag_name("span")[0].text
-        if park_to_check.pretty_name in park_name:
+        if park_to_check.value in park_name:
             return park_availability.get_attribute("class") == "available"
     return False
 
--- parks.py
+++ parks.py
@@ -1,23 +1,11 @@
 from enum import Enum
 
 
-class Parks(Enum):
-    MAGIC_KINGDOM = 1, "Magic Kingdom"
-    HOLLYWOOD_STUDIOS = 2, "Hollywood Studios"
-    EPCOT = 3, "Epcot"
-    ANIMAL_KINGDOM = 4, "Animal Kingdom"
-
-    def __new__(cls, *args, **kwargs):
-        park = object.__new__(cls)
-        park._value_ = args[0]
-        return park
-
-    def __init__(self, _: str, pretty_name: str = None):
-        self._pretty_name_ = pretty_name
+class Park(Enum):
+    MAGIC_KINGDOM = "Magic Kingdom"
+    HOLLYWOOD_STUDIOS = "Hollywood Studios"
+    EPCOT = "Epcot"
+    ANIMAL_KINGDOM = "Animal Kingdom"
 
     def __str__(self):
         return self.value
-
-    @property
-    def pretty_name(self):
-        return self._pretty_name_
